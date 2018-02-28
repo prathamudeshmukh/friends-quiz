47b8747e16118f37ff76bc3ec4b0c1d6f5d0ad2a
Administrator <admin@example.com>
2018-02-28 11:50:43 +0530

    Initial Commit

---
+++ src/main/java/com/friends/friendsquiz/FriendsQuizApplication.java
@@ -0,0 +1,12 @@
+package com.friends.friendsquiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FriendsQuizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FriendsQuizApplication.class, args);
+	}
+}
